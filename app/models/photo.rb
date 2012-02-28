d4366f80f4e71f391e8e8ae4edadf76f188a23e3
Rick Grundy <rick@rickgrundy.com>
2012-02-28 14:56:04 +0800

    Use CDN for photos in production.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -9,6 +9,8 @@ class Photo < ActiveRecord::Base
       :access_key_id => ENV["S3_KEY"],
       :secret_access_key => ENV["S3_SECRET"]
     },
+    :s3_host_alias => Rails.env.production? ? 'cdn.rickgrundy.com' : nil,
+    :url => ':s3_alias_url',
     :bucket => "rickgrundy-#{Rails.env}",
     :path => "images/:style/:id.:extension",
     :styles => { 
